ecc29bbd68fc72c499e9686cc0367bff15629245
David Meijer <david-meijer@live.nl>
2023-03-21 00:05:37 +0100

    ENH: inference script now handles input files with multiple queries

--- inference.py
+++ inference.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import typing as ty 
 import argparse 
 import os
 
@@ -10,22 +11,28 @@ def cli() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-i", "--input", type=str, required=True,
-        help="Journal article abstract.")
+        help="Input txt file with abstract per line.")
     parser.add_argument(
         "-m", "--model_dir", type=str, required=True,
         help="Path to model dir.")
     return parser.parse_args()
 
 
-def inference(model, query: str) -> float:
-    return tf.sigmoid(model(tf.constant([query]), training=False)).numpy()[0][0]
+def inference(model, queries: ty.List[str]) -> ty.List[float]:
+    return tf.sigmoid(model(tf.constant(queries), training=False)).numpy()[0]
 
 
 def main() -> None:
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
     args = cli()
+
+    queries = []
+    with open(args.input, "r") as fo:
+        for line in fo:
+            queries.append(line.strip())
+
     model = tf.saved_model.load(args.model_dir)
-    print(inference(model, args.input))
+    print(inference(model, queries))
+    
     exit(0)
 
 
